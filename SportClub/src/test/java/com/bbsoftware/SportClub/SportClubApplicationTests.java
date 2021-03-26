52b64c0874ddf52ed2143123ce08c61d75ff14f1
Jakub Rychlicki <kuba8rychlicki@gmail.com>
2021-03-26 19:00:34 +0100

    Initial commit

---
+++ SportClub/src/test/java/com/bbsoftware/SportClub/SportClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbsoftware.SportClub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
