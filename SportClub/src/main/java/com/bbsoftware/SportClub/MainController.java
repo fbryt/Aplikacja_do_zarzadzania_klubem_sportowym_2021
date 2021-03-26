31e2466f9bc1bc5850f003539551ba088f8e760e
Jakub Rychlicki <kuba8rychlicki@gmail.com>
2021-03-26 23:00:34 +0100

    h2-console enabled. First controller added

---
+++ SportClub/src/main/java/com/bbsoftware/SportClub/MainController.java
@@ -0,0 +1,14 @@
+package com.bbsoftware.SportClub;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "Witajcie sportowe swiry";
+    }
+}
---
+++ SportClub/src/main/java/com/bbsoftware/SportClub/SecurityConfiguration.java
@@ -0,0 +1,16 @@
+package com.bbsoftware.SportClub;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/").permitAll().and()
+                .authorizeRequests().antMatchers("/console/**").permitAll();
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+}
