aeca2e36e1f9e0d3845d496cb121201a6fb5de4d
Jakub Rychlicki <kuba8rychlicki@gmail.com>
2021-03-26 23:49:39 +0100

    fix

--- SportClub/src/main/java/com/bbsoftware/SportClub/SecurityConfiguration.java
+++ SportClub/src/main/java/com/bbsoftware/SportClub/SecurityConfiguration.java
@@ -1,14 +1,16 @@
 package com.bbsoftware.SportClub;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/").permitAll().and()
-                .authorizeRequests().antMatchers("/console/**").permitAll();
+                .authorizeRequests().antMatchers("/h2-console/**").permitAll();
 
         http.csrf().disable();
         http.headers().frameOptions().disable();
