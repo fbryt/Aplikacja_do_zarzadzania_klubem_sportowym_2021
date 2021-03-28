4d149ad32eb8d8863f26139c2a1f19439e31cdf3
Krzysztof Bański <36855672+KrzBan@users.noreply.github.com>
2021-03-28 22:58:10 +0200

    Update App.test.js
    case insenitive text search

--- SportClub-ui/src/App.test.js
+++ SportClub-ui/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText('Order list');
+  const linkElement = screen.getByText('order list', { exact: false } );
   expect(linkElement).toBeInTheDocument();
 });
