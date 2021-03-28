9092af81dabbb90bd423238adf1ca531300f1b1f
Krzysztof Bański <36855672+KrzBan@users.noreply.github.com>
2021-03-28 22:52:04 +0200

    Update App.test.js
    Updated test

--- SportClub-ui/src/App.test.js
+++ SportClub-ui/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText('Orders');
+  const linkElement = screen.getByText('Order list');
   expect(linkElement).toBeInTheDocument();
 });
