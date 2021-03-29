ea61c7fbf2b25aadc0ec17727912acab29be4ff3
Krzysztof Bański <krzys0100@wp.pl>
2021-03-29 14:26:08 +0200

    MainControllerTest symbol fix

--- SportClub-api/src/test/java/com/bbsoftware/SportClub/MainControllerTest.java
+++ SportClub-api/src/test/java/com/bbsoftware/SportClub/MainControllerTest.java
@@ -2,13 +2,15 @@ package com.bbsoftware.SportClub;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+import com.bbsoftware.SportClub.controllers.MainController;
+
 public class MainControllerTest {
 
     @Test
-    void hello()
-    {
-        MainController controller=new MainController();
-        String response= controller.hello();
-        assertEquals("Witajcie sportowe swiry",response);
+    void hello() {
+        MainController controller = new MainController();
+        String response = controller.hello();
+        assertEquals("Witajcie sportowe swiry", response);
     }
 }
