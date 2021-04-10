cf8d21507aa29368f90dd63a4a9fab59b00e94eb
Krzysztof Bański <krzys0100@wp.pl>
2021-04-10 13:39:41 +0200

    Implemented AppUser controller, and patch request

---
+++ SportClub-api/src/main/java/com/bbsoftware/SportClub/exceptions/AppUserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.bbsoftware.SportClub.exceptions;
+
+public class AppUserNotFoundException extends RuntimeException {
+
+  public AppUserNotFoundException(Long id) {
+    super("Could not find app user " + id);
+  }
+}
