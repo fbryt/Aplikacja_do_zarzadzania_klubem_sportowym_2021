1fb2e277a293ff187f348cda8e844103e8e2eaac
P1nkman1337 <47832248+P1nkman1337@users.noreply.github.com>
2021-04-12 11:20:25 +0200

    Login Service Tests

--- SportClub-api/src/test/java/com/bbsoftware/SportClub/appuser/AppUserServiceTest.java
+++ SportClub-api/src/test/java/com/bbsoftware/SportClub/appuser/AppUserServiceTest.java
@@ -1,8 +1,6 @@
 package com.bbsoftware.SportClub.appuser;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -12,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 @TestInstance(Lifecycle.PER_CLASS)
@@ -21,7 +20,6 @@ public class AppUserServiceTest{
 
 
     private static AppUserRepository repository;
-
     @BeforeAll
     void init(){
         repository = mock(AppUserRepository.class,CALLS_REAL_METHODS);
@@ -35,15 +33,24 @@ public class AppUserServiceTest{
     }
 
    /* @Test
-    @Disabled
     void testsignUpUser(){
 
         AppUser user = new AppUser("xyz","yzx","example12@example.com","password",AppUserRole.PLAYER);
-        when(repository.save(user)).thenCallRealMethod();
         when(repository.findByEmail("example12@example.com")).thenCallRealMethod();
         service.signUpUser(user);
         UserDetails details = service.loadUserByUsername("example12@example.com");
         assertEquals("example12@example.com",details.getUsername());
+    }*/
+
+    /*@Test
+    void testSignInUser(){
+        AppUser user = new AppUser("xyz","zyx","email","password",AppUserRole.PLAYER);
+        when(repository.findByEmail("email")).thenReturn(Optional.of(user));
+        //when(repository.findByEmail("email").isPresent()).thenReturn(true);
+        //when(repository.findByEmail("email").get()).thenReturn(user);
+        String expectedValue = "email";
+        String actualValue = service.signInUser("email","password");
+        assertTrue(actualValue.contains(expectedValue));
     }*/
 
 }
