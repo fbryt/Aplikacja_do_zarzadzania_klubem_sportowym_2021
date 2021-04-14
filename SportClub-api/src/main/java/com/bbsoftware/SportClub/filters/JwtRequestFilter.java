1069dd78b88b207224ff547d76899d4a3d70a380
Michał Bernacki <michal.bernacki121@gmail.com>
2021-04-14 21:35:48 +0200

    JwtFilter added

--- SportClub-api/src/main/java/com/bbsoftware/SportClub/filters/JwtRequestFilter.java
+++ SportClub-api/src/main/java/com/bbsoftware/SportClub/filters/JwtRequestFilter.java
@@ -1,42 +1,35 @@
 package com.bbsoftware.SportClub.filters;
 
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
-import lombok.AllArgsConstructor;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.bbsoftware.SportClub.utils.JwtUtil;
-import com.bbsoftware.SportClub.appuser.AppUserService;
-
 import java.io.IOException;
 
 @Component
-@AllArgsConstructor
 public class JwtRequestFilter extends OncePerRequestFilter {
 
-    private final AppUserService userDetailsService;
 
-    private final JwtUtil jwtUtil;
+    @Autowired
+    private MyUserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
 
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
-            FilterChain filterChain) throws ServletException, IOException {
+                                    FilterChain filterChain) throws ServletException, IOException {
 
-        final String authorizationHeader = httpServletRequest.getHeader("Authorization");
+        final String authorizationHeader = httpServletRequest.getHeader("Authorisation");
 
         String username = null;
         String jwt = null;
 
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+        if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")){
             jwt = authorizationHeader.substring(7);
             username = jwtUtil.extractUsername(jwt);
         }
