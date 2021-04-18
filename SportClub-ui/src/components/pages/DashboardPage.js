762b083c51aea31516db4ef34c4fceb283bad800
Krzysztof Bański <krzys0100@wp.pl>
2021-04-18 23:03:17 +0200

    Staged Admin Menu

--- SportClub-ui/src/components/pages/DashboardPage.js
+++ SportClub-ui/src/components/pages/DashboardPage.js
@@ -1,12 +1,10 @@
 import React from "react";
 import MenuAdmin from "../MenuAdmin";
-import MenuCoach from "../MenuCoach";
 import MenuPlayer from "../MenuPlayer";
 
 const DashboardPage = () => (
   <div>
     <MenuPlayer />
-    <MenuCoach />
     <MenuAdmin />
   </div>
 );
