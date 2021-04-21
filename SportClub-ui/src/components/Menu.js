59445d83e867b6af1e7eff12a32b3f11f88335fe
Filip Brytan <f.brytan@o2.pl>
2021-04-21 22:55:11 +0200

    [ADZKS-72] Gradient created and added to application

--- SportClub-ui/src/components/Menu.js
+++ SportClub-ui/src/components/Menu.js
@@ -1,16 +1,6 @@
-import {
-  Navbar,
-  Nav,
-  Form,
-  FormControl,
-  Button,
-  Dropdown,
-  DropdownButton,
-} from "react-bootstrap";
-
 import MenuPlayer from './MenuPlayer';
-import MenuAdmin from "./MenuAdmin";
-import MenuCoach from "./MenuCoach";
+import MenuAdmin from './MenuAdmin';
+import MenuCoach from './MenuCoach';
 import {Cookies} from 'react-cookie';
 import {Cryptr} from 'cryptr';
 
@@ -18,19 +8,17 @@ const Menu = () => {
      const cookie=new Cookies();
         const Cryptr=require('cryptr');
         const cryptr=new Cryptr('Secret');
-        var role=cryptr.decrypt(cookie.get('role'));
-        let n;
-        if(role=='COACH')
-        {
+        const role=cryptr.decrypt(cookie.get('role'));
+        let n = 0;
+        if(role==='COACH')
             n=<MenuCoach />;
-        }else if(role=='PLAYER')
-        {
+        else if(role==='PLAYER')
          n=<MenuPlayer />;
-        } else if(role=='ADMIN') n=<MenuAdmin />;
-  return (
-    <div>
-        {n};
-    </div>
+        else if(role==='ADMIN') n=<MenuAdmin />;
+          return (
+            <div>
+                {n}
+            </div>
   );
 };
 export default Menu;
