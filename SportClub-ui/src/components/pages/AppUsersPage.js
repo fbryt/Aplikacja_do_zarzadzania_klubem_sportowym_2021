1878b287e1cf7f7bf58ed2277376b6fe81f1d7cc
Krzysztof Bański <krzys0100@wp.pl>
2021-05-01 13:00:22 +0200

    [ADZKS-95] Refactored the table

--- SportClub-ui/src/components/pages/AppUsersPage.js
+++ SportClub-ui/src/components/pages/AppUsersPage.js
@@ -4,11 +4,17 @@ import Menu from "../menu/Menu";
 import Footer from "../menu/Footer";
 import axios from "axios";
 import { Container, Table, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import BootstrapTable from 'react-bootstrap-table-next';
+import cellEditFactory, { Type } from 'react-bootstrap-table2-editor';
+import filterFactory, { textFilter } from 'react-bootstrap-table2-filter';
+import paginationFactory from 'react-bootstrap-table2-paginator';
 
 const url = "http://localhost:8080/appUsers/";
 
 export const AppUserPage = () => {
+
+    const history = useHistory();
 
     const [data, setData] = useState([]);
     useEffect(async () => {
@@ -21,11 +27,90 @@ export const AppUserPage = () => {
 
     }, []);
 
+    const columns = [{
+        dataField: 'id',
+        text: 'User ID',
+        sort: true
+    }, {
+        dataField: 'firstName',
+        text: 'First Name',
+        sort: true,
+        filter: textFilter()
+    }, {
+        dataField: 'lastName',
+        text: 'Last Name',
+        sort: true,
+        filter: textFilter()
+    }, {
+        dataField: 'email',
+        text: 'Email',
+        sort: true,
+        filter: textFilter()
+    }, {
+        dataField: 'appUserRole',
+        text: 'Role',
+        sort: true,
+        editor: {
+            type: Type.SELECT,
+            options: [{
+                value: 'COACH',
+                label: 'Coach'
+            }, {
+                value: 'PLAYER',
+                label: 'Player'
+            }, {
+                value: 'ADMIN',
+                label: 'Admin'
+            }]
+        }
+    }];
+
+    const defaultSorted = [{
+        dataField: 'id',
+        order: 'asc'
+    }]
+
+    const onCellSave = async (oldValue, newValue, row, column) => {
+        const update = {
+            firstName: row.firstName,
+            lastName: row.lastName,
+            email: data.email,
+            appUserRole: row.appUserRole
+        }
+        console.log(update);
+        try {
+            const url = "http://localhost:8080/appUsers/" + row.id;
+            await axios.patch(url, update);
+            history.push(`/appUsers`)
+        } catch (e) {
+            console.log(`😱 Axios request failed: ${e}`);
+        }
+    }
+
     return (
         <div>
             <Menu />
             <Container text style={{ marginTop: '7em' }}>
-                <h1>App Users</h1>
+                <BootstrapTable
+                    bootstrap4
+                    keyField="id"
+                    data={data}
+                    columns={columns}
+                    defaultSorted={defaultSorted}
+                    noDataIndication="Table is Empty"
+                    filter={filterFactory()}
+                    pagination={paginationFactory()}
+                    cellEdit={cellEditFactory({
+                        mode: 'click',
+                        blurToSave: true,
+                        afterSaveCell: onCellSave
+                    })}
+                />
+
+                {
+
+
+                /* <h1>App Users</h1>
                 <Table className="my-gradient2" responsive striped borderless hover style={{ borderRadius: '10px' }}>
                     <thead className="my-gradient">
                         <tr>
@@ -45,7 +130,7 @@ export const AppUserPage = () => {
                             })
                         }
                     </tbody>
-                </Table>
+                </Table> */}
             </Container>
             <Footer />
         </div >
