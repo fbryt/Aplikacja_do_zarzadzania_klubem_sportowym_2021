462cd4a27209b41a7ea6122ea0f33bd043eee4f4
rriczku <kuba8rychlicki@gmail.com>
2021-05-12 18:09:02 +0200

    [ADZKS-115] Panel do wyswietlania ogloszen

--- SportClub-ui/src/components/AnnCard.js
+++ SportClub-ui/src/components/AnnCard.js
@@ -1,11 +1,28 @@
 import React from "react";
-
+import { Button} from "react-bootstrap"
 export const AnnCard =({announcement})=>{
 
+    function edit(){
+        console.log("Edit")
+    }
+    function remove(){
+        console.log("Remove")
+    }
     return (
         <div id="logform">
-            <h5>{announcement.user.firstName} {announcement.user.lastName}</h5>
-            <div>{announcement.text}</div>
+            <div class="row">
+                <div><h5>{announcement.user.firstName} {announcement.user.lastName}</h5></div>
+                <div class="date">{announcement.date.substring(0,10)+" "+announcement.date.substring( 11,16)}</div>
+            </div>
+            <div>
+            <span class="text">{announcement.text}</span>
+            </div>
+        <Button variant="primary" size="sm" onClick={edit}>
+            Edit
+        </Button>{" "}
+            <Button variant="primary" size="sm" onClick={remove}>
+                Delete
+            </Button>
         </div>
     );
 
