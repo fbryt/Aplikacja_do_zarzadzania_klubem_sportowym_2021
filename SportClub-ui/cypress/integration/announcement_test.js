08939a1a792f3ccb9eb8f5fa570c07632cc456b3
rriczku <kuba8rychlicki@gmail.com>
2021-05-14 23:53:14 +0200

    Tests Announcements front

--- SportClub-ui/cypress/integration/announcement_test.js
+++ SportClub-ui/cypress/integration/announcement_test.js
@@ -17,4 +17,26 @@ describe('Create announcement', () => {
         cy.contains('11.05.2021 Na Sohan odbedzie sie event gorniczy');
     })
 })
+describe('Edit and delete announcement', () => {
+    it('Is able to edit and delete announcement', () => {
+
+        cy.login(currentUser);
+
+        cy.visit('http://localhost:3000/dashboard');
+
+        cy.get('ul button:first').click({multiple : true});
+
+        cy.url().should('include', '/dashboard');
+
+        cy.contains('11.05.2021 Na Sohan odbedzie sie event gorniczy');
+        cy.get('textarea').type("siema");
+        cy.get('button').contains("Save Changes").click({multiple:true});
+        cy.url().should('include', '/dashboard');
+        cy.contains('11.05.2021 Na Sohan odbedzie sie event gorniczysiema');
+        cy.get('button').contains("Delete").click({multiple : true});
+        cy.url().should('include', '/dashboard');
+        cy.contains('11.05.2021 Na Sohan odbedzie sie event gorniczy').should('not.exist');
+    })
+})
+
 
