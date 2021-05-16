c141ef89b7021adcdee2565c8b9032fa07cec800
P1nkman1337 <47832248+P1nkman1337@users.noreply.github.com>
2021-05-16 21:42:13 +0200

    [ADZKS-17] contract tests cypress

---
+++ SportClub-ui/cypress/integration/addcontract_test.js
@@ -0,0 +1,47 @@
+const currentUser = {
+    username: "pablo@white.com",
+    password: "pablowhite"
+}
+const nextUser =
+
+    describe('Check contractPage', () => {
+        it('Is able to edit contract data', () => {
+
+            cy.login(currentUser);
+
+            cy.visit('http://localhost:3000/appUsers');
+
+            cy.contains('Pablo');
+            cy.contains('Black');
+            cy.get('tbody').within(($list) => {
+                cy.get('tr').eq(4).within(() =>{
+                    cy.get('td').eq(5).within(()=>{
+                        cy.get('button').click();
+                    })
+                })
+            })
+            cy.get('form').within(() =>{
+                cy.contains("Create New Contract").click();
+                cy.get('input[name="money"]').clear().type(6000);
+                cy.get(".react-date-picker__inputGroup").eq(1).within(()=>{
+                    cy.get('input[name="year"]').type(2022);
+                })
+                cy.contains("Save Changes").click();
+            })
+
+            cy.visit('http://localhost:3000/appUsers');
+            cy.contains('Pablo');
+            cy.contains('Black');
+            cy.get('tbody').within(($list) => {
+                cy.get('tr').eq(4).within(() =>{
+                    cy.get('td').eq(5).within(()=>{
+                        cy.get('button').click();
+                    })
+                })
+            })
+            cy.get('form').within(() =>{
+                cy.get('input[name="money"]').invoke('val').should('eq','6000');
+            })
+
+        })
+    })
---
+++ SportClub-ui/cypress/integration/editcontract_test.js
@@ -0,0 +1,42 @@
+const currentUser = {
+    username: "pablo@white.com",
+    password: "pablowhite"
+}
+const nextUser =
+
+describe('Check contractPage', () => {
+    it('Is able to edit contract data', () => {
+
+        cy.login(currentUser);
+
+        cy.visit('http://localhost:3000/appUsers');
+
+        cy.contains('Pablo');
+        cy.contains('Black');
+        cy.get('tbody').within(($list) => {
+            cy.get('tr').eq(3).within(() =>{
+                cy.get('td').eq(5).within(()=>{
+                    cy.get('button').click();
+                })
+            })
+        })
+        cy.get('form').within(() =>{
+            cy.get('input[name="money"]').clear().type(3000);
+            cy.contains("Save Changes").click();
+        })
+
+        cy.visit('http://localhost:3000/appUsers');
+        cy.contains('Pablo');
+        cy.contains('Black');
+        cy.get('tbody').within(($list) => {
+            cy.get('tr').eq(3).within(() =>{
+                cy.get('td').eq(5).within(()=>{
+                    cy.get('button').click();
+                })
+            })
+        })
+        cy.get('form').within(() =>{
+            cy.get('input[name="money"]').invoke('val').should('eq','3000');
+        })
+    })
+})
