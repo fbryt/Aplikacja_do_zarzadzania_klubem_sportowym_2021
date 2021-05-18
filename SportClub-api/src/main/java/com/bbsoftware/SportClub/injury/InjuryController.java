d9bcd58e060198e8385b7a8fe2aac17eff1ae089
rriczku <kuba8rychlicki@gmail.com>
2021-05-18 11:54:32 +0200

    [ADZKS-147] Kontroler kontuzji beta

---
+++ SportClub-api/src/main/java/com/bbsoftware/SportClub/injury/InjuryController.java
@@ -0,0 +1,106 @@
+package com.bbsoftware.SportClub.injury;
+
+import com.bbsoftware.SportClub.announcement.*;
+import com.bbsoftware.SportClub.appuser.AppUser;
+import com.bbsoftware.SportClub.appuser.AppUserRepository;
+import com.bbsoftware.SportClub.exceptions.AnnouncementNotFoundException;
+import com.bbsoftware.SportClub.exceptions.AppUserNotFoundException;
+import io.jsonwebtoken.io.IOException;
+import lombok.AllArgsConstructor;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.File;
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+@AllArgsConstructor
+public class InjuryController {
+    private final InjuryRepository injuryRepository;
+    private final InjuryModelAssembler assembler;
+    private final AppUserRepository appUserRepository;
+
+
+    @GetMapping("/injuries")
+    public CollectionModel<EntityModel<Injury>> all() {
+        List<EntityModel<Injury>> injuries = injuryRepository.findAll().stream()
+                .map(assembler::toModel).collect(Collectors.toList());
+
+        return CollectionModel.of(injuries, linkTo(methodOn(InjuryController.class).all()).withSelfRel());
+    }
+
+    @GetMapping("/injuries/{id}")
+    public EntityModel<Announcement> one(@PathVariable Long id) {
+        Injury injury = injuryRepository.findById(id)
+                .orElseThrow(() -> new InjuryNotFoundException(id));
+        return assembler.toModel(injury);
+    }
+
+    @PostMapping("/injuries")
+    ResponseEntity<EntityModel<Announcement>> newAnnouncement(@RequestBody InjuryRequest injuryRequest) {
+
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof AppUser) {
+
+            Long id = ((AppUser) principal).getId();
+            Injury injury = new Injury();
+            injury.setDescription(injuryRequest.getText());
+            injury.setStart_date(injuryRequest.getStart_date());
+            injury.setEnd_date(injuryRequest.getEnd_date());
+            AppUser appUser = appUserRepository.findById(id).orElseThrow(() -> new AppUserNotFoundException(id));
+            injury.setUser(appUser);
+
+            injuryRepository.save(injury);
+
+
+            return ResponseEntity //
+                    .created(linkTo(methodOn(InjuryController.class).one(injury.getId())).toUri()) //
+                    .body(assembler.toModel(injury));
+
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+        }
+
+    }
+
+    @DeleteMapping("/injuries/{id}/delete")
+    public void delete(@PathVariable Long id) {
+        Injury injury = injuryRepository.findById(id) //
+                .orElseThrow(() -> new InjuryNotFoundException(id));
+
+        injury.delete(injury);
+    }
+
+    @PatchMapping("/injuries/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Map<Object, Object> updates) {
+
+        Injury injury = injuryRepository.findById(id) //
+                .orElseThrow(() -> new InjuryNotFoundException(id));
+
+        updates.forEach((k, v) -> {
+            // use reflection to get field k on manager and set it to value v
+            Field field = ReflectionUtils.findField(Injury.class, (String) k);
+            field.setAccessible(true);
+            ReflectionUtils.setField(field, injury, v);
+        });
+
+        Injury updatedInjury = injuryRepository.save(injury);
+        return ResponseEntity //
+                .created(linkTo(methodOn(InjuryController.class).one(updatedInjury.getId())).toUri()) //
+                .body(assembler.toModel(updatedInjury));
+    }
+}
